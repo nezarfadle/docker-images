aff7c6d5ccc0a8e0266a4a77dd931087e6291770
Nezar Fadle <nezarfadle@gmail.com>
2018-02-12 15:02:51 +0800

    update help menu

--- Tools/help.sh
+++ Tools/help.sh
@@ -2,9 +2,12 @@
 (
  printf 'Tool\tVersion\n'
  printf '%s\t%s\t%s\n' "----" "-------"
+ printf '%s\t%s\t%s\n' "bash" "4.4.19"
+ printf '%s\t%s\t%s\n' "make" "4.2.1"
  printf '%s\t%s\t%s\n' "php" "7.1.12"
  printf '%s\t%s\t%s\n' "composer" "1.6.3"
  printf '%s\t%s\t%s\n' "git" "2.15.0"
+ printf '%s\t%s\t%s\n' "git-ftp" "1.4.0"
  printf '%s\t%s\t%s\n' "node" "v8.9.3"
  printf '%s\t%s\t%s\n' "npm" "5.5.1"
  printf '%s\t%s\t%s\n' "curl" "7.57.0"
